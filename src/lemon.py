bbd21a3b4042677f474ae163333ffea51144a674
pointThink <69512713+pointThink@users.noreply.github.com>
2021-01-01 19:41:00 +0100

    New commands

--- src/lemon.py
+++ src/lemon.py
@@ -5,6 +5,14 @@ import getpass
 import subprocess
 import sys
 import shutil
+import platform
+import psutil
+import win32api
+import win32file
+import socket
+from get_nic import getnic
+import datetime
+# wow that's a lot of imports
 
 processRunning = False
 mode = "Lemon"
@@ -15,7 +23,7 @@ print("")
 
 while processRunning == False:
 
-    print(color(getpass.getuser(), fg="cyan"), end=" ")
+    print(color(platform.node() + " | " + getpass.getuser(), fg="cyan"), end=" ")
     print(color(os.getcwd(), fg="yellow"))
 
     command = input(color(mode + " > ", fg="green"))
@@ -33,6 +41,7 @@ while processRunning == False:
         except:
             pass
 
+    print()
     # general commands
     if command == "exit":
         sys.exit(1)
@@ -61,12 +70,12 @@ while processRunning == False:
             print("Mode = " + mode)
 
         # file managment commands
-        elif keyWord == "showdir": # printing directory contents
+        elif keyWord == "ls": # printing directory contents
             try:
                 if parameter == None:
                     dirList = os.listdir(os.getcwd()) # getting list of contents
                 else:
-                    dirList = os.listdir(command[8:])
+                    dirList = os.listdir(command[3:])
             except:
                 print(color("Directiry is non existent or permission was denied", fg="red"))
 
@@ -106,9 +115,9 @@ while processRunning == False:
             except:
                 print(color("Failed to kill " + command[8:], fg="red"))
 
-        elif keyWord == "showtasks":
+        elif keyWord == "tasklist":
             output = os.popen('wmic process get description, processid').read()
-            print(output)
+            print(output, end="")
 
         elif keyWord == "start": # starting programs
             try:
@@ -132,6 +141,107 @@ while processRunning == False:
 
                 except:
                     print(color("Failed to copy file", fg="red"))
+
+        elif keyWord == "sysinfo":
+            print("Name: " + color(platform.node(), fg="orange"))
+            print("Windows Version: " + color(platform.platform(), fg="orange"))
+            print("CPU: " + color(platform.processor(), fg="orange"))
+            print("Architecture: " + color(platform.machine(), fg="orange"))
+            memory = psutil.virtual_memory()
+            print("Memory: " + color(round(memory.total / 1000000000, 1), fg="orange"))
+
+        elif keyWord == "type":
+            if command[5:] == "":
+                print("File not specified")
+            else:
+                try:
+                    file = open(command[5:])
+
+                    for line in file:
+                        print(line, end="")
+                except:
+                    print(color("Could not open file " + file, fg="red"))
+                    
+        elif keyWord == "info": # prints information about system
+            if parameter == "mem": # memory
+                 memory = psutil.virtual_memory()
+                 print("Total: " + color(round(memory.total / 1000000000, 1), fg="orange"))
+                 print()
+               
+                 print("Used: " + color(round(memory.used / 1000000000, 1), fg="orange")) # calculating used memory
+                 print("Free: " + color(round(memory.available / 1000000000, 1), fg="orange"))
+            elif parameter == "drive":
+                drives = win32api.GetLogicalDriveStrings()
+                drives = drives.split('\000')[:-1]
+                
+                try:
+                
+                    for drive in drives:
+                        print(drive, end=" ")
+                        print("info")
+                    
+                        print("Total: " + color(psutil.disk_usage(drive).total / 1000000000, fg="orange"))
+                        print("Used space: " + color(psutil.disk_usage(drive).used / 1000000000, fg="orange"))
+                        print("Free space: " + color(psutil.disk_usage(drive).free / 1000000000, fg="orange"))
+                    
+                        driveTypeInt = win32file.GetDriveType(drive)
+                    
+                        if driveTypeInt == 2:
+                            driveType = "Removable Disk"
+                        elif driveTypeInt == 3:
+                            driveType = "Local Disk"
+                        elif driveTypeInt == 4:
+                            driveType = "Network Drive"
+                        elif driveTypeInt == 5:
+                            driveType = "CD"
+                        else:
+                            driveType = "Unknown"
+                    
+                        print("Drive type: " + color(driveType, fg ="orange"))
+                        print()
+                        
+                except PermissionError:
+                        print(color("Could not read drive " + drive  + " Drive not ready", fg="red"))
+        
+            elif parameter == "net":
+                print("Name: " + color(socket.gethostname(), fg="orange"))
+                print("IP Adress: " + color(socket.gethostbyname(socket.gethostname()), fg="orange"))
+                
+                print()
+                print("NIC Cards")
+                
+
+                addrs = psutil.net_if_addrs().keys
+                
+                for nic in addrs():
+                        print(color(nic, fg="orange"))
+                
+            elif parameter == "power":
+                print("Charge: " + color(psutil.sensors_battery().percent, fg="orange"), end="")
+                print(color("%", fg="orange"))
+                
+                if psutil.sensors_battery().power_plugged == False:
+                    print("Status: " + color("Using Battery", fg="orange"))
+                    
+                elif psutil.sensors_battery().power_plugged == True:
+                    print("Status: " + color("Plugged in", fg="orange"))
+
+                else:
+                    print("Status: " + color("Unknown", fg="orange"))
+            
+            elif parameter == "cpu":
+                print("CPU: " + color(platform.processor(), fg="orange"))
+                print("Architecture: " + color(platform.machine(), fg="orange"))
+                print("Logical CPU's: " + color(psutil.cpu_count(logical=True), fg="orange"))
+                
+            
+            else:
+                print(color("Invalid parameter", fg="red"))
+            
+        elif keyWord == "time":
+            print("The current time is ", end="")
+            print(datetime.datetime.now())
+
         else:
             try:
                 subprocess.Popen(command)
@@ -144,6 +254,8 @@ while processRunning == False:
 
     elif mode == "Powershell":
         os.system("powershell /c " + command)
+        
+    print()
 
     processRunning = False
     parameter = None
